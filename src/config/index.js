c3e6c1dc98b2180bd85e184031b2b0da81361155
volyk_evheniy <evhenii_official@gmail.com>
2024-07-02 19:52:38 +0300

    changed address of mocked users service

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   // Services
-  USERS_SERVICE: 'http://localhost:1000',
+  USERS_SERVICE: 'http://localhost:3000',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
   CAR_SERVICE: 'http://localhost:1000',
 };
